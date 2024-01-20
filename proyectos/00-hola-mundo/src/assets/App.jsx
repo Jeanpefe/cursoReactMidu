442bad53ee96f4c913392d9d5ad0e77697282572
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-01-21 00:07:23 +0100

    twitter card html layout component

---
+++ proyectos/00-hola-mundo/src/assets/App.jsx
@@ -0,0 +1,16 @@
+export function App () {
+    return (
+        <article>
+            <header>
+                <img alt="avatar de Jeanepfe" src="https://unavatar.io/Jeanpefe" />
+                <div>
+                    <strong>Jesús Ángel</strong>
+                    <span>@Jeanpefe</span>
+                </div>
+            </header>
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
