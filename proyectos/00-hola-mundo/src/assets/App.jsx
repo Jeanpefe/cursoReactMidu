e6bf277a235c4d3daf2455831365733ffa7ae6e5
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-01-21 22:16:53 +0100

    App classNames

--- proyectos/00-hola-mundo/src/assets/App.jsx
+++ proyectos/00-hola-mundo/src/assets/App.jsx
@@ -1,15 +1,17 @@
+import './App.css'
+
 export function App () {
     return (
-        <article>
-            <header>
-                <img alt="avatar de Jeanepfe" src="https://unavatar.io/Jeanpefe" />
-                <div>
-                    <strong>Jesús Ángel</strong>
-                    <span>@Jeanpefe</span>
+        <article className="x_card">
+            <header className='x_card__info'>
+                <img className="x_card__image" alt="avatar de Jeanepfe" src="https://unavatar.io/Jeanpefe" />
+                <div className="x_card__text">
+                    <strong className="x_card__name">Jesús Ángel</strong>
+                    <span className="x_card__username">@Jeanpefe</span>
                 </div>
             </header>
-            <aside>
-                <button>Seguir</button>
+            <aside className="x_card__aside">
+                <button className="x_card__follow">Seguir</button>
             </aside>
         </article>
     )
