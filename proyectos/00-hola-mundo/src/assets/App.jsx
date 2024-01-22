7f65e61967f18d5da8bc320c59e528b1aa0e3d8c
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-01-22 18:33:18 +0100

    App container styles instead that on component

--- proyectos/00-hola-mundo/src/assets/App.jsx
+++ proyectos/00-hola-mundo/src/assets/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import { TwitterFollowCard } from './TwitterFollowCard'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App () {
     return (
-        <>
+        <section className='App'>
             <TwitterFollowCard userName="Jeanpefe" name="Jesús Ángel" />
-            <TwitterFollowCard userName="Jeanpefe" name="Jesús Ángel" />
-            <TwitterFollowCard userName="Jeanpefe" name="Jesús Ángel" />
-        </>
+            <TwitterFollowCard userName="paula1610" name="Paula" />
+            <TwitterFollowCard userName="snakeyesmp" name="Snakeyesmp" />
+        </section>
     )
 }
