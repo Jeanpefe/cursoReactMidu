77367a067f0cb565c271a9298398c6f77c525ea9
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-01-25 22:55:56 +0100

    initialState common error

--- proyectos/00-hola-mundo/src/assets/App.jsx
+++ proyectos/00-hola-mundo/src/assets/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react"
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App () {
     const formatUserName = (userName) => `@${userName}`
-	const jeanpefe = {userName: "Jeanpefe", formatUserName: formatUserName, initialIsFollowing: true} //No suele ser buena practica, por temas de rerenderizado y comprension
+	const [isFollowing, setIsFollowing] = useState(false)
+	//Este estado se pasa como prop despues a jeanpefe, pero al pulsar el boton no se cambia porque en el hijo se usa como estado inicial, SOLO SE RENDERIZA UNA VEZ. El estado inicial solo se inicia una vez
+	const jeanpefe = {userName: "Jeanpefe", formatUserName: formatUserName, initialIsFollowing: isFollowing} //No suele ser buena practica, por temas de rerenderizado y comprension
 	//Cuando queremos pasar una prop para inicializar un estado es buena practica poner initial delante
+	//
     return (
         <section className='App'>
             <TwitterFollowCard {...jeanpefe}>
@@ -22,6 +26,7 @@ export function App () {
                 userName="snakeyesmp">
                 Snakeyesmp
             </TwitterFollowCard>
+			<button onClick={() => setIsFollowing(!isFollowing)} >Intento cambiar el estado</button>
         </section>
     )
 }
