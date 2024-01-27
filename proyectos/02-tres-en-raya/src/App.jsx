90a84e42c43c5290397a10eee83239d551fa929c
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-01-27 23:58:49 +0100

    check winner

--- proyectos/02-tres-en-raya/src/App.jsx
+++ proyectos/02-tres-en-raya/src/App.jsx
@@ -20,15 +20,27 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 	)
 }
 
+const WINNER_COMBOS = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
+
 function App() {
 	const [board, setBoard] = useState(Array(9).fill(null))
 
 	const [turn, setTurn] = useState(TURNS.X)
+	const [winner, setWinner] = useState(null) // null no winner, false -> empate
 
 	const updateBoard = (index) => {
 		// no actualizar si ya hay value
 		if (board[index]) return
-		
+
 		const newBoard = [...board]
 		newBoard[index] = turn
 		setBoard(newBoard)
@@ -36,6 +48,18 @@ function App() {
 
 		const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
 		setTurn(newTurn)
+	}
+
+	const checkWinner = (board) => {
+		for (const combo of WINNER_COMBOS) {
+			const [a, b, c] = combo
+			if (
+				board[a] &&
+				board[a] === board[b] &&
+				board[a] === board[c]
+			)
+			return board[a]
+		}
 	}
 	return (
 		<main className='board'>
