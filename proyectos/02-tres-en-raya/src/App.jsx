54329f1c512bbdaeec01a177c847d018474d97ba
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-01-30 20:18:50 +0100

    constants.js file

--- proyectos/02-tres-en-raya/src/App.jsx
+++ proyectos/02-tres-en-raya/src/App.jsx
@@ -2,22 +2,7 @@ import { useState } from "react";
 import confetti from "canvas-confetti";
 import './App.css'
 import { Square } from "./components/Square.jsx"
-const TURNS = {
-	X: 'x',
-	O: 'o'
-}
-
-
-const WINNER_COMBOS = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[2, 4, 6]
-]
+import { TURNS, WINNER_COMBOS } from "./constants.js";
 
 function App() {
 	const [board, setBoard] = useState(Array(9).fill(null))
