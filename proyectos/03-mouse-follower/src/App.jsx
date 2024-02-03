0c13ef0c980eb60ca9f040253f4b78cb70db1eca
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-02-03 23:12:57 +0100

    basic useEffect, useState and onClick event

--- proyectos/03-mouse-follower/src/App.jsx
+++ proyectos/03-mouse-follower/src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const [enabled, setEnabled] = useState(false)
+  useEffect(() => {
+    console.log("hola")
+  })
   return (
-    <h1>Proyecto 3</h1>
+    <>
+      <h1>Proyecto 3</h1>
+      <button onClick={() => (!enabled)}>
+          {enabled ? 'Desactivar' : 'Activar'} Seguir el puntero
+        </button>
+    </>
   )
 }
 
