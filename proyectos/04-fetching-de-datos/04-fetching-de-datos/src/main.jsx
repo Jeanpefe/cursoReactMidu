376d5b1152a3036b8ad73e642e2de90330500a09
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-02-07 19:01:27 +0100

    almost empty files with minimum code to show smth

--- proyectos/04-fetching-de-datos/04-fetching-de-datos/src/main.jsx
+++ proyectos/04-fetching-de-datos/04-fetching-de-datos/src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
