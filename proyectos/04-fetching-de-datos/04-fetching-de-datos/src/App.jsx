f50cd7878b0c2e233a863b52499551728c5ed19a
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-02-08 22:34:25 +0100

    fetch image from first word of the first fetch

--- proyectos/04-fetching-de-datos/04-fetching-de-datos/src/App.jsx
+++ proyectos/04-fetching-de-datos/04-fetching-de-datos/src/App.jsx
@@ -1,21 +1,34 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 const CAT_ENDPOINT_FACT = 'https://catfact.ninja/fact'
-// const CAT_ENDPOINT_IMAGE = `https://cataas.com/cat/says/${word}?size=50&color=red&json=true` 
+// const CAT_ENDPOINT_IMAGE = `https://cataas.com/cat/says/${word}?fontSize=50&fontColor=red&json=true` 
 
 function App() {
   const [fact, setFact] = useState()
+  const [catId, setCatId] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_FACT)
       .then(res => res.json())
-      .then(data => setFact(data.fact))
+      .then(data => {
+        const {fact} = data //Desesctructuramos la respuesta y obtenemos la propiedad fact
+        setFact(fact)
+
+        const firstWord = fact.split(' ')[0]
+        fetch(`https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`)
+        .then(res => res.json())
+        .then(resJson => {
+          const {_id} = resJson
+          setCatId(_id)
+        })
+      })
   }, [])
 
   return (
     <main>
       <h1>Gatos</h1>
       {fact && <p>{fact}</p>}
+      {}
     </main>
   )
 }
