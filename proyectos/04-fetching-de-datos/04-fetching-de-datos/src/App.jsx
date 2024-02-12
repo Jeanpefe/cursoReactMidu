d66915be26a20db6902ef7762f68d5c40735f0e5
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-02-12 22:29:32 +0100

    first custom hook

--- proyectos/04-fetching-de-datos/04-fetching-de-datos/src/App.jsx
+++ proyectos/04-fetching-de-datos/04-fetching-de-datos/src/App.jsx
@@ -4,16 +4,11 @@ import './App.css'
 const CAT_ENDPOINT_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE = `https://cataas.com/cat/says/${word}?fontSize=50&fontColor=red&json=true` 
 
-function App() {
-  const [fact, setFact] = useState()
+//La diferencia entre un custom hook y una funcion es que en una funcion no puedes llamar a hooks
+function useCatImage({ fact }) { // custom hook necesita empezar por la palabra reservada use
+  //Pasamos { fact } y no fact porque así fuerzas a pasar el mismo nombre en el param, (no puedes pasar { factTest }, 
+    //y porque mejora la escalabilidad, ya que en un futuro podemos recibir varios parametros {fact, limitWords,...} y es más legible el codigo y no importa el orden
   const [catId, setCatId] = useState()
-  
-  // recuperar la cita al cargar la pagina
-  useEffect(async () => {
-    const newFact = await getRandomFact()
-    setFact(newFact)
-  }, [])
-
   // recuperar la imagen cada vez que tenemos una cita
   useEffect(() => {
     if (!fact) return
@@ -24,6 +19,19 @@ function App() {
       setCatId(_id)
     })
   },[fact])
+
+  return { catId }
+}
+
+export function App() {
+  const [fact, setFact] = useState()
+  const { catId } = useCatImage({ fact })
+
+  // recuperar la cita al cargar la pagina
+  useEffect(async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }, [])
 
   const handleClick = async () => {
     getRandomFact().then(newFact => setFact(newFact)) //Esto es lo mismo que getRandomFact().then(setFact)
@@ -40,5 +48,3 @@ function App() {
     </main>
   )
 }
-
-export default App
