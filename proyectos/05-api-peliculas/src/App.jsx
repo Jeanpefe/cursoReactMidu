6026cea44e31f1024b4c7f60034d5021b2509d0d
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-02-16 23:40:53 +0100

    movies preview

--- proyectos/05-api-peliculas/src/App.jsx
+++ proyectos/05-api-peliculas/src/App.jsx
@@ -1,20 +1,40 @@
 import { useState } from 'react'
 import './App.css'
+import responseMovies from './mocks/api-results.json'
+import withoutResults from './mocks/api-no-results.json'
 
 function App() {
-
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
   return (
     <div className='page'>
       <header>
         <h1>Buscador de peliculas</h1>
         <form className='form'>
           <input placeholder='Bee Movie' />
-          <button type='submit'>Buscar</button>
+          <button type='submit'>Buscar</button>****
         </form>
       </header>
 
       <main>
-        Aquí habrá cosas
+        {
+          hasMovies
+          ? (
+            <ul>
+              {
+                movies.map(movie => (
+                  <li key={movie.Title}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                  </li>
+                ))
+              }
+            </ul>
+          )
+          : (
+            <p>No hay datos</p>
+          )
+        } 
       </main>
     </div>
   )
