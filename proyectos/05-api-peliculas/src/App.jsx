f5f0e339ae5087460b644b37f3f2b1bb8cd0cbaf
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-02-20 22:05:07 +0100

    data validation

--- proyectos/05-api-peliculas/src/App.jsx
+++ proyectos/05-api-peliculas/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {useRef} from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
@@ -7,6 +7,8 @@ import { useMovies } from './hooks/useMovies'
 function App() {
   const {movies} = useMovies()
   const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
+
   // Forma controlada: cada vez que cambia el texto del input se renderiza todo otra vez, es una forma más lenta. Pero facilita la validación del formulario
   const handleSubmit = (event) => {
     event.preventDefault() //evitar que se envie el formulario de la forma estándar, pudiendo definir logica custom
@@ -14,8 +16,29 @@ function App() {
   }
 
   const handleChange = (event) => {
-    setQuery(event.target.value) // Con el .target accedemos al objetivo del evento
+    const newQuery = event.target.value // Con el .target accedemos al objetivo del evento
+    //Si no creamos esta variable podemos tener problemas de asincronia
+    setQuery(newQuery) 
+    if (newQuery === '') {
+      setError("No se puede buscar una pelicula vacia")
+      return
+    }
+
+    if (newQuery.match(/^\d+$/)) {
+      setError('No se puede buscar solo con un numero')
+      return
+    }
+
+    if (newQuery.length < 4) {
+      setError("Introduce al menos 4 caracteres")
+      return
+    }
+    setError(null)
   }
+
+  useEffect(() => {
+
+  }, [query])
 
   return (
     <div className='page'>
@@ -25,6 +48,7 @@ function App() {
           <input onChange={handleChange} value={query} name="query" placeholder='Bee Movie' />
           <button type='submit'>Buscar</button>****
         </form>
+        {error && <p style={{color: 'red'}}>{error}</p>}
       </header>
 
       <main>
