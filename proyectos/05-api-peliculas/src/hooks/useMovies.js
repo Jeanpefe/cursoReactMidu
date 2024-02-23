b46e5fd1693e3fa0a0f5ad9928af660247a8776d
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-02-23 23:10:03 +0100

    service to fetch movies

--- proyectos/05-api-peliculas/src/hooks/useMovies.js
+++ proyectos/05-api-peliculas/src/hooks/useMovies.js
@@ -1,25 +1,15 @@
 import withResults from '../mocks/api-results.json'
 import withoutResults from '../mocks/api-no-results.json'
+import { searchMovies } from '../services/movies'
+
 import { useState } from 'react'
 
 export function useMovies ({search}) {
-    const [responseMovies, setResponseMovies] = useState([])
+    const [movies, setMovies] = useState([])
     
-    const movies = responseMovies.Search
-  
-    const mappedMovies = movies?.map(movie => ({
-      id: movie.imdbID,
-      title: movie.Title,
-      year: movie.Year,
-      poster: movie.Poster
-    }))
-
-    const getMovies = () => {
-      if (search) {
-        setResponseMovies(withResults)
-      } else {
-        setResponseMovies(withoutResults)
-      }
+    const getMovies = async () => {
+      const newMovies = await searchMovies({search})
+      setMovies(newMovies)
     } 
-    return {movies: mappedMovies, getMovies}
+    return {movies, getMovies}
   }
