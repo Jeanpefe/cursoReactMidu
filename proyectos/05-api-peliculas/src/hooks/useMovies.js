994003c4b3941b9addaf59fc6b4b145a1b2f24a8
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-02-24 23:50:30 +0100

    bad alternative to useRef to save previousSearch

--- proyectos/05-api-peliculas/src/hooks/useMovies.js
+++ proyectos/05-api-peliculas/src/hooks/useMovies.js
@@ -4,14 +4,15 @@ import { searchMovies } from '../services/movies'
 
 import { useState, useRef } from 'react'
 
+let previousSearch = '' //Esto funciona porque solo usamos el custom hook una vez, porque la variable se comparte allá donde pongamos el compoennte
+
 export function useMovies ({search}) {
     const [movies, setMovies] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
-    const previousSearch = useRef(search)
 
     const getMovies = async () => {
-      if (search == previousSearch.current) return 
+      if (search == previousSearch) return 
 
       try{
         setLoading(true)
