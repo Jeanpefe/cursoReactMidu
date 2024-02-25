ea2d54ed202574185db24783990f626fe4820da4
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-02-25 22:02:31 +0100

    useMemo to sort movies when needed + useMemo readme

--- proyectos/05-api-peliculas/src/hooks/useMovies.js
+++ proyectos/05-api-peliculas/src/hooks/useMovies.js
@@ -1,6 +1,6 @@
 import { searchMovies } from '../services/movies'
 
-import { useState, useRef } from 'react'
+import { useState, useRef, useMemo } from 'react'
 
 export function useMovies ({search, sort}) {
     const [movies, setMovies] = useState([])
@@ -22,15 +22,20 @@ export function useMovies ({search, sort}) {
       } finally {
         setLoading(false)
       }
-    } 
-    console.log(movies)
+    }
     
-    if (!movies) {
-      return {movies, loading, getMovies}
-    } 
-    const sortedMovies = sort
-    ? [...movies].sort((a,b) => a.title.localeCompare(b.title)) 
-    : movies 
+    // if (!movies) {
+    //   return {movies, loading, getMovies}
+    // } 
+    // const sortedMovies = sort
+    // ? [...movies].sort((a,b) => a.title.localeCompare(b.title)) 
+    // : movies 
+
+    const sortedMovies = useMemo(() => {
+      return sort
+      ? [...movies].sort((a,b) => a.title.localeCompare(b.title)) 
+      : movies
+    }, [sort, movies]) // El useMemo nos sirve para hacer esa operación solo si ha cambiado el sort o el movies, sino se haría cada vez que se rerenderiza el componente y por tanto el cuerpo de la funcion
 
     return {movies:sortedMovies, loading, getMovies}
   }
