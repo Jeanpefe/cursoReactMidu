0f21401a870346bd9845afdaa43753ce31a7c225
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-03-04 23:11:10 +0100

    feat: context to store filters useState

--- proyectos/06-carrito-compra/src/context/filters.jsx
+++ proyectos/06-carrito-compra/src/context/filters.jsx
@@ -1,15 +1,19 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
-//Creamos el coontexto
+//Creamos el coontexto. Es el que tenemos que consumir
 export const FiltersContext = createContext()
 
-//Creamos el Provider del contexto
+//Creamos el Provider del contexto. Es el que nos provee de acceso
 export function FiltersProvider({children}) {
+    const [filters, setFilters] = useState({
+        category: 'all',
+        minPrice: 0        
+    })
     return (
         <FiltersContext.Provider value={{
             //Aquí va la información a la que queramos acceder
-            category: 'all',
-            minPrice: 0
+            filters,
+            setFilters
         }}>
             {children}
         </FiltersContext.Provider>
