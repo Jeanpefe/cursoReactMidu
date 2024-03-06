14e9722ee8e5ccd3d3f97148cba2d81e2e608303
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-03-06 23:55:37 +0100

    fix: single source of the truth

--- proyectos/06-carrito-compra/src/components/Filters.jsx
+++ proyectos/06-carrito-compra/src/components/Filters.jsx
@@ -3,14 +3,13 @@ import './Filters.css'
 import { useFilters } from '../hooks/useFilters'
 
 export function Filters () {
-    const {setFilters} = useFilters()
+    const {filters, setFilters} = useFilters()
 
     const [minPrice, setMinPrice] = useState(0)
     const minPriceFilterId = useId()
     const categoryFilterId = useId()
 
     const handleChangeMinPrice = (event) => {
-        setMinPrice(event.target.value)
         setFilters(prevState => ({
             ...prevState,
             minPrice: event.target.value
@@ -36,8 +35,9 @@ export function Filters () {
                     min='0'
                     max='1000'
                     onChange={handleChangeMinPrice}
+					value={filters.minPrice}
                 />
-                <span>€{minPrice}</span>
+                <span>€{filters.minPrice}</span>
             </div>
 
             <div>
