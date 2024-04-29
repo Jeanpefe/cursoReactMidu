55b14900c4ef33781e2f1a6a92db9fdf29fe590d
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-04-29 23:29:53 +0200

    refactor: provider in separated function

--- proyectos/06-carrito-compra/src/context/cart.jsx
+++ proyectos/06-carrito-compra/src/context/cart.jsx
@@ -3,23 +3,28 @@ import { initialState, cartReducer } from "../reducers/cart";
 
 export const CartContext = createContext()
 
-export function CartProvider ({children}) {
+function useCartReducer() {
 	const [state, dispatch] = useReducer(cartReducer, initialState)
-
+	
 	const addToCart = product => dispatch({
 		type: 'ADD_TO_CART',
 		payload: product
 	})
-
+	
 	const removeFromCart = product => dispatch({
 		type: 'REMOVE_FROM_CART',
 		payload: product
 	})
-
+	
 	const clearCart = () => dispatch({
 		type: 'CLEAR_CART',
 	})
 
+	return {state, addToCart, removeFromCart, clearCart}
+}
+
+export function CartProvider ({children}) {
+	const {state, addToCart, removeFromCart, clearCart} = useCartReducer()	
 	return (
         <CartContext.Provider value={{
             cart: state,
