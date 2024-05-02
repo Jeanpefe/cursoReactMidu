7a43b521fb87c6d0da0d780d6ca0778136d09606
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-05-02 20:48:57 +0200

    first aproach to routing: conditional rendering with state and window.location.pathname

--- proyectos/07-react-router/src/App.jsx
+++ proyectos/07-react-router/src/App.jsx
@@ -1,12 +1,32 @@
 import { useState } from 'react'
 import './App.css'
 
+function HomePage() {
+	return (
+		<>
+		<h1>Home</h1>
+		<a href='/about'>Sobre nosotros</a>
+		</>
+	)
+}
+
+function AboutPage() {
+	return (
+		<>
+		<h1>About</h1>
+		<a href='/'>Ir a la home</a>
+		</>
+	)
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+	const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
   return (
-    <>
-    </>
+	<main>
+		{currentPath === '/' && <HomePage />}
+		{currentPath === '/about' && <AboutPage />}
+	</main>
   )
 }
 
