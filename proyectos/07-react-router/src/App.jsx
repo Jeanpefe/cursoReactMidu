f06601698e35b5f7ffc2e2779671e68fbf6e0cf4
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-05-12 12:24:37 +0200

    chore: lazy loading of AboutComponent

--- proyectos/07-react-router/src/App.jsx
+++ proyectos/07-react-router/src/App.jsx
@@ -1,10 +1,13 @@
+import {lazy} from 'react'
+
 import './App.css'
-import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
+import HomePage from './pages/HomePage' //import estático
 import { Router } from './Router'
 import { Page404 } from './pages/404'
 import { SearchPage } from './pages/SearchPage'
 import { Route } from './Route'
+
+const LazyAboutPage = lazy(() => import ('./pages/About.jsx')) //import dinámico
 
 const appRoutes = [
 	{
@@ -18,7 +21,7 @@ function App() {
 	<main>
 		<Router routes={appRoutes} defaultComponent={Page404}>
 			<Route path='/' Component={HomePage} />
-			<Route path='/about' Component={AboutPage} />
+			<Route path='/about' Component={LazyAboutPage} />
 		</Router>
 
 	</main>
