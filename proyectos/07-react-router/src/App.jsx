4019df0a748a1713f5b302df60fbbed7a5a6bf29
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-05-13 22:53:40 +0200

    feat: multiple lang with route param

--- proyectos/07-react-router/src/App.jsx
+++ proyectos/07-react-router/src/App.jsx
@@ -1,18 +1,23 @@
 import {Suspense, lazy} from 'react'
 
 import './App.css'
-import HomePage from './pages/HomePage' //import estático
+// import HomePage from './pages/HomePage' //import estático
 import { Router } from './Router'
 import { Page404 } from './pages/404'
-import { SearchPage } from './pages/SearchPage'
 import { Route } from './Route'
 
 const LazyAboutPage = lazy(() => import ('./pages/AboutPage.jsx')) //import dinámico (Lazy Loading)
+const LazySearchPage = lazy(() => import ('./pages/SearchPage.jsx')) //import dinámico (Lazy Loading)
+const LazyHomePage = lazy(() => import ('./pages/HomePage.jsx')) //import dinámico (Lazy Loading)
 
 const appRoutes = [
 	{
 		path: '/search/:query',
-		Component: SearchPage
+		Component: LazySearchPage
+	},
+	{
+		path: '/:lang/about',
+		Component: LazyAboutPage
 	}
 	]
 
@@ -21,7 +26,7 @@ function App() { //Ponemos el Suspense porque no sabemos si se ha cargado el Laz
 	<main>
 		<Suspense fallback={<div>Cargando...</div>}> 
 			<Router routes={appRoutes} defaultComponent={Page404}>
-				<Route path='/' Component={HomePage} />
+				<Route path='/' Component={LazyHomePage} />
 				<Route path='/about' Component={LazyAboutPage} />
 			</Router>
 		</Suspense>
--- proyectos/07-react-router/src/pages/AboutPage.jsx
+++ proyectos/07-react-router/src/pages/AboutPage.jsx
@@ -1,10 +1,29 @@
 import {Link} from '../Link.jsx'
 
-export default function AboutPage() {
+const i18n = {
+	es: {
+		title: 'Sobre nosotros',
+		description: 'Descripción en español',
+		button: 'Ir a la home'
+	},
+	en: {
+		title: 'About us',
+		description: 'English description',
+		button: 'Ir a la home'
+	}
+}
+
+const useI18n = (lang) => {
+	return i18n[lang] || i18n.en
+}
+
+export default function AboutPage({routeParams}) {
+	const i18n = useI18n(routeParams.lang ?? 'es')
 	return (
 		<>
-		<h1>About</h1>
-		<Link to='/'>Ir a la home</Link>
+		<h1>{i18n.title}</h1>
+		<p>{i18n.description}</p>
+		<Link to='/'>{i18n.button}</Link>
 		</>
 	)
 }
