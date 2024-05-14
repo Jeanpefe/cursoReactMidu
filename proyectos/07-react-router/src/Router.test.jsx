baa4b8220db3e68ebccc6509a840d8528bc63e4b
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-05-14 22:21:37 +0200

    test: clean screen after each test

--- proyectos/07-react-router/src/Router.test.jsx
+++ proyectos/07-react-router/src/Router.test.jsx
@@ -1,8 +1,12 @@
-import {describe, it, expect} from 'vitest'
-import {render, screen} from '@testing-library/react'
+import {describe, it, expect, beforeEach} from 'vitest'
+import {render, screen, cleanup} from '@testing-library/react'
 import { Router } from './Router'
 
 describe('Router', () => {
+    beforeEach(() => { //limpia la pantalla antes de cada test
+        cleanup()
+    })
+
     it('should render without problems', () => {
         render(<Router routes={[]} />) //probamos a renderizar sin nada
         expect(true).toBeTruthy()
@@ -10,5 +14,6 @@ describe('Router', () => {
     it('should render 404 if no routes match', () => {
         render(<Router routes={[]} defaultComponent={() => <h1>404</h1>} />)
         console.log(screen.debug()) //hacemos un console log del html renderizado en la pantalla
+        expect(screen.getByText('404')).toBeTruthy()
     })
 })
