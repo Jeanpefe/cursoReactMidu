b663c26d3d3e12e457af1502f9a990fc922fbb12
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-05-23 23:33:56 +0200

    visual: change interchange button to a svg

--- proyectos/08-google-translate-clone/src/App.tsx
+++ proyectos/08-google-translate-clone/src/App.tsx
@@ -1,8 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import { useStore } from './hooks/useStore';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import { AUTO_LANGUAGE } from './constants';
+import { ArrowIcon } from './components/Icons';
 
 function App() {
   const { fromLanguage, toLanguage, setFromLanguage, interchangeLanguages } = useStore()
@@ -19,9 +20,9 @@ function App() {
             {fromLanguage}
           </Col>
           <Col>
-            <button disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}>
-              Interchange Languages
-            </button>
+            <Button variant="link" disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}>
+              <ArrowIcon />
+            </Button>
           </Col>
           <Col>
             <h2>To</h2>
