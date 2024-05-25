adcf0e0eb796897c12c201fcb3b3c0a370cb86ac
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-05-26 00:40:50 +0200

    refactor: alternative type with function component

--- proyectos/08-google-translate-clone/src/components/LanguageSelector.tsx
+++ proyectos/08-google-translate-clone/src/components/LanguageSelector.tsx
@@ -1,11 +1,13 @@
 import { Form } from "react-bootstrap"
 import { SUPPORTED_LANGUAGES } from "../constants"
+import {type FC } from 'react' //function component significa
 
+//Tipado con interfaces
 interface Props {
 	onChange: (language: string) => void
 }
 
-export const LanguageSelector = ({ onChange }: Props) => {
+export const LanguageSelector: FC<Props> = ({ onChange }) => { //le indicamos a TS que es un componente funcional cuyas props son las que vienen en la interfaz
     return (
         <Form.Select aria-label="Selecciona el idioma" onChange={onChange}>
             {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => ( //Se usa el Object.entries para obtener las llaves y los valores de un objeto. Si fuese un array en vez de un objeto se podría hacer el map directamente
