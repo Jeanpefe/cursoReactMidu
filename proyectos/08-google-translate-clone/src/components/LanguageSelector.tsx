0d614649d7ada76261d6f367b429ca9e1af1922e
Jeanpefe <jesusangelpequenofernandez@gmail.com>
2024-05-26 00:04:28 +0200

    chore: onChange function type on LanguageSelector

--- proyectos/08-google-translate-clone/src/components/LanguageSelector.tsx
+++ proyectos/08-google-translate-clone/src/components/LanguageSelector.tsx
@@ -1,7 +1,11 @@
 import { Form } from "react-bootstrap"
 import { SUPPORTED_LANGUAGES } from "../constants"
 
-export const LanguageSelector = ({ onChange }) => {
+interface Props {
+	onChange: (language: string) => void
+}
+
+export const LanguageSelector = ({ onChange }: Props) => {
     return (
         <Form.Select aria-label="Selecciona el idioma" onChange={onChange}>
             {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => ( //Se usa el Object.entries para obtener las llaves y los valores de un objeto. Si fuese un array en vez de un objeto se podría hacer el map directamente
